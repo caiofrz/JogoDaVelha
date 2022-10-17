830cfd1ad9515d71a84868339c9f1fc81b74528a
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2022-10-17 15:51:22 -0300

    add computadorFacil.jogar e mudança jogador.jogar

--- src/ComputadorFacil.java
+++ src/ComputadorFacil.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Random;
+
 public class ComputadorFacil extends Jogador{ 
 
     public ComputadorFacil(String nickName, String simbolo) {
@@ -7,8 +9,22 @@ public class ComputadorFacil extends Jogador{
     }
 
     @Override
-    public void jogar(){
+    public void jogar(String[][] tabuleiro){
+        Random aleatorio = new Random(3);
 
+        int linha, coluna;
+
+        while(true){
+            linha = aleatorio.nextInt(0, 2);
+            coluna = aleatorio.nextInt(0, 2);
+
+            if(tabuleiro[linha][coluna] == " "){
+                tabuleiro[linha][coluna] = super.getSimbolo();
+                return;
+            }
+        }
+
+        
 
     }
 
--- src/Jogador.java
+++ src/Jogador.java
@@ -12,7 +12,7 @@ public class Jogador{
     }
 
 
-    public void jogar(){
+    public void jogar(String[][] tabuleiro){
 
     }
 
