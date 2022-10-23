3fee19f55387a72cf69cf5eb156299baa28cf08c
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2022-10-23 00:40:18 -0300

    add instruções ,apresentarGanhador() + fluxo main

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,12 @@
 package src;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 
 public class Principal {
+
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -11,8 +14,10 @@ public class Principal {
         System.out.println("\t\t\t\tJOGO DA VELHA");
         System.out.println("\t\t\t------------------------------");
 
-        Tabuleiro tabuleiro = new Tabuleiro();
-        /*Scanner input = new Scanner(System.in);
+        Tabuleiro tabuleiro = new Tabuleiro();//Criando meu  tabuleiro
+        Boolean vitoria = false;
+
+        menuInstrucoes();//Chamada da função que irá mostrar as instruções do jogo
         
 
         System.out.println("INFORME SEU NICK: ");
@@ -21,104 +26,77 @@ public class Principal {
         System.out.println("INFORME O SÍMBOLO QUE DESEJA JOGAR (X ou O): ");
         String simbolo = input.next();
 
-        Jogador jogador1 = new Jogador(nickName, simbolo);
+        Jogador jogador1 = new Jogador(nickName, simbolo);//Criando o jogador 1, com base no nick e simbolo escolhidos
         
+        jogador1.escolherOponente(); //Faz a chamada do menu, para o jogador escolher seu oponente/nível de jogo
 
-        EscolherOponente(jogador1); //Mostra o menu de dificuldades na tela*/
+        Jogador jogador2 = jogador1.getOponente();
 
-        tabuleiro.mostrarTabuleiro();
+        //iniciando o jogo
+        while(!vitoria){
 
+            if (tabuleiro.getQuantidadeJogadas() == 9) { //se o tabuleiro estiver completo o jogo acabou
+                break;
+            }
+
+            tabuleiro.mostrarTabuleiro();
+
+            jogador1.jogar(tabuleiro.getPosicoesTabuleiro());
+            System.out.println("1");
+            vitoria = tabuleiro.verificarVitoria();
+            System.out.println("2");
+            tabuleiro.setQuantidadeJogadas();      
+            System.out.println("3");
+
+            tabuleiro.mostrarTabuleiro();
+
+            jogador2.jogar(tabuleiro.getPosicoesTabuleiro());
+            System.out.println("4");
+            vitoria = tabuleiro.verificarVitoria(); 
+            System.out.println("5");
+            tabuleiro.setQuantidadeJogadas();    
+            System.out.println("6");
+
+        }
+
+        apresentarGanhador(tabuleiro, jogador1, jogador2);
+        input.close();
         
     }
 
 
+    public static void apresentarGanhador(Tabuleiro tabuleiro, Jogador jogador1, Jogador jogador2){
 
-
-
-    public static void EscolherOponente(Jogador jogador1){ //Função que verifica o oponente do jogador
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("VAI JOGAR SOZINHO? ");
-
-        System.out.println("\n");
-        System.out.println("1. SIM");
-        System.out.println("2. NÃO");
-        System.out.println("\n");
-
-        System.out.println("ESCOLHA: ");
-        int oponente = input.nextInt();
-
-        switch(oponente){
-
-            case 1:
-                // Caso o oponente seja o computador, é feita a chamada da função de escolha de dificuldade 
-                menuDificuldade(jogador1);
-                break;
-
-            case 2:
-                // Caso tenha 2 jogadores, essa estrura faz a instancia de um novo jogador
-                System.out.println("INFORME O NICK: ");
-                String nickName = input.next();
-
-                String simbolo = jogador1.getSimbolo() == "X" ? "O" : "X";
-                Jogador jogador2 = new Jogador(nickName, simbolo);
-                break;
-
-            default:
-                System.out.println("ESCOLHA INVÁLIDA");
-        }
-
-        limparTela();
-
-    }
-
-
-
-
-
-
-
-
-    public static void menuDificuldade(Jogador jogador1){ //Função responsável pela escolha do nível do jogo
-
-        Scanner input = new Scanner(System.in);
-
-        String simbolo = jogador1.getSimbolo() == "X" ? "O" : "X";
-
-
-            System.out.println("--------------------------------");
-            System.out.println("ESCOLHA A DIFICULDADE DO JOGO!");
-            System.out.println("--------------------------------");
-
-            System.out.println("1. Fácil");
-            System.out.println("2. Difícil");
-            System.out.println("\n");
-
-            System.out.println("INFORME O NUMERO EQUIVALENTE A SUA ESCOLHA: ");
-
-            int dificuldade = input.nextInt();
-            
-        while(dificuldade !=1 && dificuldade !=2){ //Loop de validação da escolha do nível
-
-            System.out.println("ESCOLHA INVÁLIDA! INFORME A NOVA ESCOLHA: ");
-            dificuldade = input.nextInt();
-        }
-
-        if (dificuldade == 1) {// Estrutura que instancia um jogador-computador de acordo o nível de dificuldade
-            ComputadorFacil jogador2 = new ComputadorFacil("pcFacil", simbolo); 
+        if(tabuleiro.verificarVitoria() == false && tabuleiro.getQuantidadeJogadas() == 9 ){
+            JOptionPane.showMessageDialog(null, "EMPATE");
+        } else if(tabuleiro.getSimboloVencedor() == jogador1.getSimbolo()){
+            JOptionPane.showMessageDialog(null, "Vencedor: " + jogador1.toString());
         } else {
-             ComputadorDificil jogador2 = new ComputadorDificil("pcDificil", simbolo);
-        }        
+            JOptionPane.showMessageDialog(null, "Vencedor: " + jogador2.toString());
+        }
     }
 
 
+    public static void menuInstrucoes(){
 
+        System.out.println("--------------------------------");
+        System.out.println("\tINSTRUÇÕES");
+        System.out.println("--------------------------------");
+
+        System.out.println("1. Este jogo é um jogo de console! Portanto todas as ações serão feitas através do mesmo");
+        System.out.println("2. O tabuleiro funcionará como um plano cartesiano");
+        System.out.println("3. As jogadas serão feitas por coordenadas de linha e coluna");
+        System.out.println("4. As linhas vão de 0 a 2 e de cima para baixo, onde 0 é a linha mais acima");
+        System.out.println("5. As colunas vão de 0 a 2 e da esquerda para a direta, onde 0 é a coluna mais a esquerda");
+        System.out.println("6. EX. de jogada: linha = 0 e coluna = 0");
+        System.out.println("7. Com esse exemplo de jogada você irá marcar o primeiro espaço mais a esquerda e mais acima do tabuleiro");
+        System.out.println();
+    }
 
 
 
     public final static void limparTela() { // Função responsável por "limpar a tela"
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println();
         }
     }
